20165582ee4086b91920412a510396e959b36c11
Alex.A.Om.temp <Alex.A.Om.temp@gmail.com>
2026-04-11 21:17:40 +0300

    layers fix

--- Assets/Scripts/Player/Poison.cs
+++ Assets/Scripts/Player/Poison.cs
@@ -15,7 +15,7 @@ public class Poison : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("poison") || collision.gameObject.CompareTag("faire") )
+        if (collision.gameObject.CompareTag("poison") || collision.gameObject.CompareTag("fire") )
         {
             rb.linearVelocity = Vector3.zero;
             Invoke(nameof(Restart), 1f);
