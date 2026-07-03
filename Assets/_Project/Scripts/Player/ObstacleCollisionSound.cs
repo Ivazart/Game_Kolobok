210b387a1d36da380eb31211a2889e240c86a954
Alex.A.Om.temp <Alex.A.Om.temp@gmail.com>
2026-07-03 15:26:09 +0300

    sounds added

---
+++ Assets/_Project/Scripts/Player/ObstacleCollisionSound.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Global;
+using UnityEngine;
+
+namespace _Project.Player
+{
+    public class ObstacleCollisionSound : MonoBehaviour
+    {
+        [Serializable]
+        public struct SceneSound
+        {
+            public SceneName scene;
+            public AudioClip clip;
+        }
+        
+        [SerializeField] private SceneSound[] sceneSounds;
+        [SerializeField] private List<string> obstacleTags;
+        [SerializeField] private float cooldown = 0.5f; // секунд между повторными звуками с одного коллайдера
+
+        private readonly Dictionary<Collider2D, float> lastCollisionTimes = new();
+        private SceneController sceneController => SceneController.Instance;
+        private AudioManager audioManager => AudioManager.Instance;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            foreach (string obstacleTag in obstacleTags)
+            {
+                if (collision.gameObject.CompareTag(obstacleTag))
+                {
+                    if (CanPlaySound(collision.collider))
+                        PlaySceneSound();
+                    break; // достаточно одного совпадения
+                }
+            }
+        }
+
+        private bool CanPlaySound(Collider2D collider)
+        {
+            if (collider == null) return false;
+            float now = Time.time;
+            if (lastCollisionTimes.TryGetValue(collider, out float lastTime))
+            {
+                if (now - lastTime < cooldown)
+                    return false;
+            }
+            lastCollisionTimes[collider] = now;
+            return true;
+        }
+        
+        private void PlaySceneSound()
+        {
+            if (sceneController == null) return;
+
+            SceneName currentScene = sceneController.CurrentScene;
+
+            foreach (var ss in sceneSounds)
+            {
+                if (ss.scene == currentScene)
+                {
+                    if (ss.clip != null)
+                        audioManager.PlaySFX(ss.clip);
+                    return;
+                }
+            }
+        }
+
+        // Опционально: очистка старых записей, если коллайдеры уничтожаются/переиспользуются
+        private void OnDisable() => lastCollisionTimes.Clear();
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerSounds.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Global;
+using UnityEngine;
+
+namespace _Project.Player
+{
+    public class PlayerSounds : MonoBehaviour
+    {
+        [SerializeField] private AudioClip deathSoundPoison;
+        [SerializeField] private AudioClip deathSoundFire;
+        [SerializeField] private AudioClip deathSoundSwamp;
+
+        private AudioManager audioManager => AudioManager.Instance;
+        private GameController gameController => GameController.Instance;
+
+        private void Start()
+        {
+            gameController.OnPlayerDeath += OnPlayerDeath;
+        }
+
+        private void OnPlayerDeath(DeathType deathType)
+        {
+            AudioClip deathSound = deathType switch
+            {
+                DeathType.Poison => deathSoundPoison,
+                DeathType.Fire => deathSoundFire,
+                DeathType.Swamp => deathSoundSwamp,
+                _ => throw new ArgumentOutOfRangeException(nameof(deathType), deathType, null)
+            };
+
+            if (deathSound != null)
+                audioManager.PlaySFX(deathSound);
+        }
+
+        private void OnDestroy()
+        {
+            try
+            {
+                gameController.OnPlayerDeath -= OnPlayerDeath;
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
+    }
+}
